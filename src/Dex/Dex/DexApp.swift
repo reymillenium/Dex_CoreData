745579107ed53d91f92ba95a62c9837aab17b2bb
Reinier Garcia Ramos <reymillenium@gmail.com>
2025-08-08 16:08:30 -0400

    First commit

---
+++ src/Dex/Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Reinier Garcia on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
